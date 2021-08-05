9572ce8031b971eca7ad14854c72d51f5ef0b554
jcraftmusic2000 <88302458+jcraftmusic2000@users.noreply.github.com>
2021-08-04 23:07:41 -0500

    Create quote_data.js

---
+++ quote_data.js
@@ -0,0 +1,86 @@
+function loadJSONdata(){
+			pageLayout = {
+				"cards": [
+					{
+						"cardID": "1",
+						"googleID": "1HIRHlnxJ_C8P92GAutpILE9Dtd-8BYtV"
+					},
+					{
+						"cardID": "2", 
+						"googleID": "1R76xxmQ4lWfYRmbH3bco5vGn1HPE4zEe"
+					},
+					{
+						"cardID": "3",
+						"googleID": "1His0lFCOPhB8Z00LZ-UDHgxwQQhitUFG"
+					},
+					{
+						"cardID": "4",
+						"googleID": "1tY2WO7s57eZSA8U4Up5TaVcN7ouBdb5D"
+					},
+					{
+						"cardID": "5",
+						"googleID": "1LLerrkMNUZqQCUmadisQs7G3T2O1Q9lZ"
+					},
+					{
+						"cardID": "6",
+						"googleID": "1R1zXv9MqcmK_9tuDjv6NBZH1MleilMS2"
+					},
+					{
+						"cardID": "7",
+						"googleID": "1rTECOjXDNDGXHhcuSh02GB0BjqyvZr8M"
+					},
+					{
+						"cardID": "8",
+						"googleID": "1AeGT4QVsBEbypXirmNoCuTELTo8uBSXG"
+					},
+					{
+						"cardID": "9",
+						"googleID": "1PHK3WV4F8lsF4DTQ6_bbg9HCAa4lPJnD"
+					},
+					{
+						"cardID": "10",
+						"googleID": "1QP9pobaxgPst6lvt4MZIuVmR4WqPYbXM"
+					},
+					{
+						"cardID": "11",
+						"googleID": "1mJWG_Bn6yHkQdnyK2LpSafJBy4o3GXDu"
+					},
+					{
+						"cardID": "12",
+						"googleID": "1sdWMjWdHYUdpDbO02k-stMGv-fO_c-7J"
+					},
+					{
+						"cardID": "13",
+						"googleID": "1XSXtqnQ8_gYO5HZvYUCx_ONH1_IN2s2c"
+					},
+					{
+						"cardID": "14",
+						"googleID": "1FOz7RPOQjTXHsC_e3E3xUYQD-5Vt1nhD"
+					},
+					{
+						"cardID": "15",
+						"googleID": "1fqI6x-365aAq1jxdGNpfr0m4pa9tsO7T"
+					},
+					{
+						"cardID": "16",
+						"googleID": "1V1qsU2OfuY5MgxiODPboGzX-0nt7uYoj"
+					},
+					{
+						"cardID": "17",
+						"googleID": "1S6lVBqi1CcLBcdG6AuUOSxVbLTiSVpGw"
+					},
+					{
+						"cardID": "18",
+						"googleID": "1GYinp1TT23nvnjlEelfI1bb_aES8bTYK"
+					},
+					{
+						"cardID": "19",
+						"googleID": "1_oGGQGi8CnKtA8-gF2JULf6YhH8HoNzv"
+					},
+					{
+						"cardID": "20",
+						"googleID": "1XSM1PZ80Ksn5uiJ77CpUcPh8Jv2fq5jF"
+					}
+				]
+			};
+		}
